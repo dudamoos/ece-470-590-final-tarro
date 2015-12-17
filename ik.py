3a7fa968646d6a6633591fd2986d867b255f0c1c
Wes Tarro <dudamoos@users.noreply.github.com>
2015-12-16 21:17:07 -0500

    Cleanup

--- ik.py
+++ ik.py
@@ -23,7 +23,7 @@ def get_jacobian(fk, d_theta, theta):
 
 # simple path planning - straight line towards goal
 # do more complex planning by IK solving for points along the intended path
-def get_next_point_delta(cur, goal, step, snap_region):
+def get_next_point_delta(cur, goal, step):
 	cur = np.array(cur, copy=False)
 	goal = np.array(goal, copy=False)
 	m = goal - cur
@@ -31,17 +31,6 @@ def get_next_point_delta(cur, goal, step, snap_region):
 	n = step * m_unit
 	# keep in reachable space
 	next = cur + n
-	#next_arm = next - fk_chain.K_RSP[0]
-	#if (np.linalg.norm(next_arm) >= 0.75):
-	#	print "\033[91mWent to far!\033[0m", next, "->", next_arm, "->", np.linalg.norm(next_arm)
-	#	next_arm = 0.7 * next_arm / np.linalg.norm(next_arm) # enforce reachability
-	#	next = next_arm + fk_chain.K_RSP[0]
-	#	n = next - cur
-	#elif (np.linalg.norm(next_arm) <= 0.5):
-	#	print "\033[92mHit too close!\033[0m", next, "->", next_arm, "->", np.linalg.norm(next_arm)
-	#	next_arm = 0.55 * next_arm / np.linalg.norm(next_arm) # enforce reachability
-	#	next = next_arm + fk_chain.K_RSP[0]
-	#	n = next - cur
 	if (np.linalg.norm(next) >= 0.75):
 		print "\033[91mWent to far!\033[0m", next, "->", np.linalg.norm(next)
 		next = 0.7 * next / np.linalg.norm(next) # enforce reachability
@@ -55,41 +44,25 @@ def get_next_point_delta(cur, goal, step, snap_region):
 		n = np.max([n, -0.02 * np.ones(len(n))], axis=0)
 		n = np.min([n,  0.02 * np.ones(len(n))], axis=0)
 		print "->", n
-	#for i in range(len(m)):
-	#	if (m[i] < snap_region): n[i] = m[i]
 	print "IK moving to", next, "by delta", n, " (dist:", m, ")"
 	return n
 
 def null_move(theta): pass
 
 def ik_solve(theta, goal, max_err, theta_step, e_step, fk, move=null_move):
-	global th
-	global pos
-	global iter_count
-	
-	th = theta = np.array(theta, copy=False)
+	theta = np.array(theta, copy=False)
 	goal = np.array(goal, copy=False)
-	pos = e = fk(theta)
-	last_distance = get_dist(e, goal)
-	iter_count = 0
+	e = fk(theta)
 	while (get_dist(e, goal) > max_err):
-		cur_distance = get_dist(e, goal)
-		print "IK distance", cur_distance
-		#if (cur_distance - last_distance > 0.2): # jumped farther away
-		#	import pdb; pdb.set_trace()
-		last_distance = cur_distance
+		print "IK distance", get_dist(e, goal)
 		J = get_jacobian(fk, theta_step, theta)
 		Jp = np.linalg.pinv(J)
-		d_e = get_next_point_delta(e, goal, e_step, max_err)
+		d_e = get_next_point_delta(e, goal, e_step)
 		d_theta = Jp.dot(np.transpose([d_e]))
-		th = theta = (theta + np.transpose(d_theta))[0]
-		#theta = np.max([theta, -(np.pi/2) * np.ones(len(theta))], axis=0)
-		#theta = np.min([theta,  (np.pi/2) * np.ones(len(theta))], axis=0)
-		th = theta = np.mod(theta + np.pi, 2*np.pi) - np.pi # normalize angles
-		#th = theta = np.mod(theta + np.pi/2, np.pi) - np.pi/2 # normalize angles
+		theta = (theta + np.transpose(d_theta))[0]
+		theta = np.mod(theta + np.pi, 2*np.pi) - np.pi # normalize angles
 		move(theta)
-		pos = e = fk(theta)
+		e = fk(theta)
 		print "IK now at", e, "->", theta
-		iter_count += 1
-	return theta # np.mod(theta + np.pi, 2*np.pi) - np.pi # normalize angles
+	return theta
 
--- main.py
+++ main.py
@@ -7,16 +7,6 @@ import ach
 import fk
 import fk_chain
 import ik
-#import path
-
-# assignment:
-#  robot on ground at (x,y) = (0, 0), red box located at (x,y,z) = (1.5, 0, 1)
-#  make robot walk up to red box and touch it with its right hand and remain standing
-
-# path planner plans ((hx, hy, hz), (lfx, lfy, lfz), (rfx, rfy, rfz))
-# control loop sends each path point to IK
-# IK solves for desired point from current pose
-# robot sends current pose to IK and control loop
 
 chan_state = ach.Channel(ha.HUBO_CHAN_STATE_NAME)
 chan_ref = ach.Channel(ha.HUBO_CHAN_REF_NAME)
@@ -48,20 +38,16 @@ def arm_lift():
 def get_fk_h(theta):
 	return fk.get_fk(theta, fk_chain.KRH)
 
-#REF_INTERVAL = 0.1
-REF_INTERVAL = 0.01
+REF_INTERVAL = 0.1
+#REF_INTERVAL = 0.01
 def ik_control(theta):
 	global cur_time
 	
 	ref.ref[ha.RSP] = theta[0]
 	ref.ref[ha.RSR] = theta[1]
-	#ref.ref[ha.RSY] = theta[2]
-	#if (theta[2] > 0): theta[2] = 0
 	if (theta[2] > 0): theta[2] = -theta[2]
 	ref.ref[ha.REB] = theta[2]
-	#ref.ref[ha.RWY] = theta[3]
 	ref.ref[ha.RWP] = theta[3]
-	#ref.ref[ha.RWR] = theta[4]
 	chan_ref.put(ref)
 	
 	[status, framesize] = chan_state.get(state, wait=False, last=True)
@@ -69,20 +55,12 @@ def ik_control(theta):
 	cur_time = state.time
 
 theta = [0, -np.pi / 3, 0, 0]
-#max_err = 0.01 # 1% of max radius the 2d arm can reach
-max_err = 0.035 # 5% of max radius each 3d arm can reach
 max_err = 0.15
 theta_step = 0.01
-#e_step = 0.003 # 1/2 of max_err
 e_step = 0.01
 
-goal = np.array([0.6, 0.0, 0.0]) # approximately pointing at the box
-#goal_arm = goal - fk_chain.K_RSP[0] # ensure in reachable range
-#goal_arm = 0.65 * goal_arm / np.linalg.norm(goal_arm)
-#goal = goal_arm + fk_chain.K_RSP[0] # reachable goal
-print "Goal position:", goal # approximately [ 0.60350985  0.00140394  0.        ]
-#goal = np.array([-0.65, -0.1, -0.3]) # backwards is forwards?
-# this point is approximately the result of theta = [-1.4, 0.2, 0, 0, 0, 0, 0]
+goal = np.array([0.6, 0.0, 0.0])
+print "Goal position:", goal
 
 arm_lift()
 
@@ -91,20 +69,4 @@ cur_time = state.time
 theta_end = ik.ik_solve(theta, goal, max_err, theta_step, e_step, get_fk_h, ik_control)
 print "Final theta:", theta_end
 print "Final pose:", get_fk_h(theta_end)
-#ik_control(theta_end)
-
-#def execute_path(path, fk, max_err):
-#	index = 0
-#	while True:
-#		cur_time = get_time()
-#		next_time = time + 0.1
-#		cur_pose = get_pose()
-#		
-#		if (get_dist(fk(cur_pose), fk(path[index])) < max_err):
-#			index += 1
-#			if (index >= len(path)): break
-#			send_to_ik(cur_pose, path[index])
-#		
-#		cur_time = get_time()
-#		sim_sleep(next_time - cur_time)
 
